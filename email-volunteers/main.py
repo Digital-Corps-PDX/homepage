f073024a8677fb26c4149e27382d3b9e4544ed0b
Zach Young <zacharysyoung@gmail.com>
2025-04-24 10:33:56 -0700

    factored main out

--- email-volunteers/main.py
+++ email-volunteers/main.py
@@ -20,6 +20,17 @@ from html.parser import HTMLParser
 #  A: https://stackoverflow.com/a/79235523/246801
 from airtable import Record, Table
 
+SUBJECT = "🌲 PDX Digital Corps — Thanks for your interest!"
+
+# The PDX Digital Corps base
+BASE_ID = "appWwcOT5kBnEq6P9"
+# The Volunteers table
+TABLE_ID = "tblBizheg4s11n93a"
+# The view of volunteers that have not been emailed the intro
+VIEW_ID = "viwOUapltcR0kfXof"
+
+VIEW_ID = "viwUYJjUwUNGqC4ir"  # dev-mode view of just ZachYoung_38
+
 
 logging.basicConfig(
     format="%(asctime)s %(message)s",
@@ -29,88 +40,148 @@ logging.basicConfig(
 
 
 def main():
+    smtp_token, airtable_token = get_tokens("secrets.json")
 
-    # -- Get secret tokens --------------------------------------------------------
+    html, text = get_email_bodies("body.html")
 
+    volunteers = get_volunteers(airtable_token)
+    if not volunteers:
+        logging.info("no new volunteers; done")
+        return
+
+    mail_srv = connect_to_mail_srv(smtp_token)
+
+    updates = send_emails(volunteers, mail_srv, html, text)
+
+    mail_srv.close()
+    logging.info("disconnected email server")
+
+    update_volunteers(airtable_token, updates)
+
+
+def get_tokens(fname: str) -> tuple[str, str]:
+    """
+    Read secrets JSON at fname, return smtp and airtable tokens.
+    """
     smtp_token, airtable_token = "", ""
     try:
-        with open("secrets.json") as f:
+        with open(fname) as f:
             data = json.load(f)
             smtp_token = data["SMTP_TOKEN"]
             airtable_token = data["AIRTABLE_TOKEN"]
     except Exception as e:
-        print(f"could not fetch secret tokens: {e}", file=sys.stderr)
-        exit(2)
-    logging.info("got token from secrets.json")
+        error_out(f"could not fetch secret tokens: {e}")
 
-    # -- Get the HTML file for the body, make text ----------------------------
+    logging.info("got smtp and airtable tokens")
 
-    html_body = ""
+    return smtp_token, airtable_token
+
+
+def get_email_bodies(fname: str) -> tuple[str, str]:
+    """
+    Read the HTML body at fname, convert to TXT, return
+    both HTML and TXT.
+    """
+    html = ""
     try:
-        with open("body.html") as f:
-            html_body = f.read()
+        with open(fname) as f:
+            html = f.read()
     except Exception as e:
-        print(f"could not get body.html: {e}", file=sys.stderr)
-        exit(2)
-    text_body = html_to_text(html_body)
+        error_out(f"could not get HTML: {e}")
 
-    # -- Get list of volunteers to email from Airtable ----------------------------
+    txt = ""
+    try:
+        txt = html_to_text(html)
+    except Exception as e:
+        error_out(f"could not convert HTML to text: {e}")
 
-    # The Volunteers table of the PDX Digital Corps base
-    BASE_ID = "appWwcOT5kBnEq6P9"
-    TABLE_ID = "tblBizheg4s11n93a"
-    # The view of volunteers that have not been emailed the intro
-    VIEW_ID = "viwOzosoARJrI14cw"
+    logging.info("got HTML body and converted to TXT")
+
+    return html, txt
+
+
+def get_volunteers(airtable_token: str) -> list[Record]:
+    """
+    Use airtable_token to read from a view in Airtable and get
+    a list of volunteers.
+    """
 
     table = Table(airtable_token, BASE_ID, TABLE_ID)
-    logging.info("created Airtable connection")
-    volunteers = table.fetch(view=VIEW_ID, fields=["Full Name", "Email", "Intro_emailed"])
-    logging.info(f"got {len(volunteers)} Volunteers")
 
-    # -- Log in to SMTP email server  ---------------------------------------------
+    volunteers: list[Record] = []
+    try:
+        volunteers = table.fetch(view=VIEW_ID, fields=["Full Name", "Email"])
+        logging.info(f"got {len(volunteers)} Volunteers")
+    except Exception as e:
+        error_out(f"could not get volunteers: {e}")
 
-    mail = smtplib.SMTP("smtp.protonmail.ch", 587)
-    mail.starttls()
-    logging.info("started tls")
-    mail.login("info@digitalcorpspdx.org", smtp_token)
-    logging.info("logged in to smtp server")
+    return volunteers
 
-    # -- Iterate volunteers, send  ------------------------------------------------
 
-    SUBJECT = "🌲 PDX Digital Corps — Thanks for your interest!"
+def connect_to_mail_srv(smtp_token: str) -> smtplib.SMTP:
+    mail_srv = smtplib.SMTP("smtp.protonmail.ch", 587)
+    try:
+        mail_srv.starttls()
+        logging.info("started tls")
+        mail_srv.login("info@digitalcorpspdx.org", smtp_token)
+        logging.info("connected to email server")
+    except Exception as e:
+        error_out(f"could not connect to email server: {e}")
 
-    for x in volunteers:
-        name, email = x["fields"]["Full Name"], x["fields"]["Email"]
+    return mail_srv
 
-        # dev-mode sanity check to prevent emailing real volunteers
-        if email != (want := "zacharysyoung@gmail.com"):
-            raise ValueError(f"{email} != {want}")
 
-        msg = EmailMessage()
-        msg["Subject"] = SUBJECT
-        msg["From"] = Address(display_name="PDX Digital Corps", addr_spec="info@digitalcorpspdx.org")
-        msg["To"] = Address(display_name=name, addr_spec=email)
-        msg.set_content(text_body)
-        msg.add_alternative(html_body, subtype="html")
-
-        mail.sendmail(msg["From"], msg["To"], msg.as_string())
-        logging.info(f" sent mail to {name}")
-
-    mail.close()
-    logging.info("closed connection to smtp server")
-
-    # -- Update records in Airtable  ----------------------------------------------
+def send_emails(volunteers: list[Record], mail_srv: smtplib.SMTP, html: str, text: str) -> list[Record]:
+    """
+    Iterate list of volunteers, composing emails with
+    html and text bodies, and sending with mail_srv.
+    """
 
     updates: list[Record] = []
     for x in volunteers:
-        updates.append(
-            {
-                "id": x["id"],
-                "fields": {"Intro_emailed": datetime.now(UTC).isoformat()},
-            }
-        )
-    table.patch(updates)
-    logging.info("updated Volunteers emailed-timestamp in Airtable")
+        try:
+            name, email = x["fields"]["Full Name"], x["fields"]["Email"]
+        except Exception as e:
+            logging.info(f"error: could not get name and email: {e}")
+            continue
+
+        # dev-mode sanity check to prevent emailing real volunteers
+        dev_email = "zacharysyoung@gmail.com"
+        if email != dev_email:
+            raise ValueError(f"DEV-MODE: {email} != {dev_email}")
+
+        try:
+            msg = EmailMessage()
+            msg["Subject"] = SUBJECT
+            msg["From"] = Address(display_name="PDX Digital Corps", addr_spec="info@digitalcorpspdx.org")
+            msg["To"] = Address(display_name=name, addr_spec=email)
+            msg.set_content(text)
+            msg.add_alternative(html, subtype="html")
+
+            mail_srv.sendmail(msg["From"], msg["To"], msg.as_string())
+            logging.info(f"  sent mail to {name}")
+
+            updates.append(
+                {"id": x["id"], "fields": {"Intro_emailed": datetime.now(UTC).isoformat()}},
+            )
+        except Exception as e:
+            error_out(f"could not compose and send message: {e}")
+
+    return updates
+
+
+def update_volunteers(airtable_token: str, updates: list[Record]):
+    """
+    Use airtable_token to send volunteer updates back to Airtable.
+    """
+
+    table = Table(airtable_token, BASE_ID, TABLE_ID)
+
+    try:
+        table.patch(updates)
+        logging.info("updated volunteers")
+    except Exception as e:
+        error_out(f"could not update volunteers: {e}")
 
 
 def html_to_text(html: str) -> str:
@@ -168,6 +239,14 @@ def html_to_text(html: str) -> str:
     parser = MyHTMLParser()
     parser.feed(html)
     return parser.to_string()
+
+
+def error_out(msg: str):
+    """Print msg to stderr and exit with code 2."""
+    if not msg.startswith("error: "):
+        msg = "error: " + msg
+    print(msg, file=sys.stderr)
+    exit(2)
 
 
 main()
